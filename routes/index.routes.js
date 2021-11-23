e54d3535e1102997a8708c00b9fe5a9f5d7db6fd
awahids <awahid.safhadi@gmail.com>
2021-11-24 03:35:53 +0700

    ! fix path directory  user balance

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth.routes");
-const balanceUserRouter = require("./balanceUser.routes");
+const balanceUserRouter = require("./userBalance.routes");
 
 router.use("/auth", authRouter);
 router.use("/balance", balanceUserRouter);
